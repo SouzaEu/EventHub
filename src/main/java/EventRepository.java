b08e62a1eaea3f3f60f8c170d8f1935a0f0dea55
Vinicius Souza <133525009+SouzaEu@users.noreply.github.com>
2025-04-02 21:28:40 -0300

    Delete src directory

--- src/main/java/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.eventmanagementsystem.repository;
-
-import com.example.eventmanagementsystem.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
-
